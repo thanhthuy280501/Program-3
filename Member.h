d33d3a4ba2829e8167c1e0fb01b62fddd30cff8b
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-11-09 22:37:40 -0500

    Member.h

--- Member.h
+++ Member.h
@@ -1,1 +1,20 @@
+class Member {
+private:
+    std::string name;
+    int id;
+    Book* borrowedBooks; // Array of borrowed books
+    int borrowedCount;
+
+public:
+    // Constructor, Copy Constructor, Assignment Operator, Destructor
+    Member(const std::string& n, int i) : name(n), id(i), borrowedBooks(nullptr), borrowedCount(0) {}
+
+    // Implement these functions similarly if borrowedBooks is dynamically allocated
+
+    ~Member() {
+        delete[] borrowedBooks; // Free the borrowed books array
+    }
+
+    // Other member functions...
+};
 
